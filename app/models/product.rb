4b603831961f9a413d3a73ee055b828f7eafa61e
agnieszka <agnzap@gmail.com>
2018-07-25 18:47:54 +0200

    search form for heroku

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   def self.search(search_term)
-    if Rails.env.development?
+    if Rails.env.development
       Product.where("name LIKE ?", "#{search_term}%")
     else
       Product.where("name ilike ?", "#{search_term}%")
