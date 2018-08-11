365b3592783ec1b6602c657df92cebfbe7af1906
agnieszka <agnzap@gmail.com>
2018-08-11 21:42:51 +0200

    6.10 AJAX, comment section

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,16 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
-    @comment.save
 
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
+        format.js
       else
         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
