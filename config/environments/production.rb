07321613aa2d1b5c797db4060aa07cefb2bdfa68
agnieszka <agnzap@gmail.com>
2018-08-15 12:10:01 +0200

    providing app from crashing due to invalid image url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  # Prevents the app from crashing if invalid image_url provided
+  config.assets.unknown_asset_fallback = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -100,7 +103,7 @@ Rails.application.configure do
                      :socket_failure_delay => 0.2,
                      :down_retry_delay => 60
                     }
-                    
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
